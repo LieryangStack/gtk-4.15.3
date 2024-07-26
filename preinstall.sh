48597b832fced168790b8b420facafd058025657
LieryangStack <sxlisibo@gmail.com>
2024-07-26 08:52:53 +0800

    update

---
+++ preinstall.sh
@@ -0,0 +1,15 @@
+# gtk4
+
+sudo apt install libxml2-utils # 解决 XMLLINT
+
+sudo apt install -y libcairo2-dev x11-xserver-utils libxrandr-dev libxi-dev libxcursor-dev
+sudo apt install -y libxdamage-dev libxinerama-dev highlight
+
+
+cd /usr/lib/aarch64-linux-gnu/pkgconfig
+sudo mv pixman-1.pc pixman-1.pc.backup
+
+# libadwaita
+
+sudo apt install -y libcurl4-openssl-dev libyaml-dev libzstd-dev
+sudo apt install -y gperf gettext itstool xsltproc docbook-xsl
